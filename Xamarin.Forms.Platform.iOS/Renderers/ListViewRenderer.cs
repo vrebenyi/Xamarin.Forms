87d59635c1b5b6849f77ccf1dd47e76e1fe8da5f
Rui Marinho <me@ruimarinho.net>
2018-11-13 23:34:23 +0000

    [IOS] Try remove UpdateEstimatedRowHeight

--- Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
@@ -544,7 +544,6 @@ namespace Xamarin.Forms.Platform.iOS
 			{
 				case NotifyCollectionChangedAction.Add:
 
-					Device.BeginInvokeOnMainThread(UpdateEstimatedRowHeight);
 					if (e.NewStartingIndex == -1 || groupReset)
 						goto case NotifyCollectionChangedAction.Reset;
 
