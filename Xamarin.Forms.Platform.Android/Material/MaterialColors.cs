4a154a18de023a6a83bee297f37057e090354430
ShaneN <shane94@hotmail.com>
2019-02-10 16:12:53 -0700

    [Visual] Material Pickers and setup sandbox app

--- Xamarin.Forms.Platform.Android/Material/MaterialColors.cs
+++ Xamarin.Forms.Platform.Android/Material/MaterialColors.cs
@@ -3,10 +3,10 @@
 
 #if __ANDROID__
 using Android.Content.Res;
-using AColor = Android.Graphics.Color;
+using PlatformColor = Android.Graphics.Color;
 #else
 using MaterialComponents;
-using AColor = UIKit.UIColor;
+using PlatformColor = UIKit.UIColor;
 #endif
 
 #if __ANDROID__
@@ -36,7 +36,7 @@ namespace Xamarin.Forms.Platform.iOS.Material
 
 		// State list from material-components-android
 		// https://github.com/material-components/material-components-android/blob/71694616056012fe1162adb9144be903d1e510d5/lib/java/com/google/android/material/textfield/res/values/colors.xml#L28
-		public static AColor CreateEntryFilledInputBackgroundColor(Color backgroundColor, Color textColor)
+		public static PlatformColor CreateEntryFilledInputBackgroundColor(Color backgroundColor, Color textColor)
 		{
 			var platformTextColor = GetEntryTextColor(textColor);
 
@@ -51,9 +51,9 @@ namespace Xamarin.Forms.Platform.iOS.Material
 			return ToPlatformColor(backgroundColor);
 		}
 
-		public static (AColor InlineColor, AColor FloatingColor) GetPlaceHolderColor(Color placeholderColor, Color textColor)
+		public static (PlatformColor InlineColor, PlatformColor FloatingColor) GetPlaceHolderColor(Color placeholderColor, Color textColor)
 		{
-			AColor color;
+			PlatformColor color;
 
 			if (placeholderColor == Color.Default)
 			{
@@ -71,13 +71,13 @@ namespace Xamarin.Forms.Platform.iOS.Material
 			return (inlineColor, floatingColor);
 		}
 
-		public static (AColor FocusedColor, AColor UnFocusedColor) GetUnderlineColor(Color textColor)
+		public static (PlatformColor FocusedColor, PlatformColor UnFocusedColor) GetUnderlineColor(Color textColor)
 		{
-			AColor color = GetEntryTextColor(textColor);
+			PlatformColor color = GetEntryTextColor(textColor);
 			return (color, WithAlpha(color, kFilledTextFieldIndicatorLineAlpha));
 		}
 
-		public static AColor GetEntryTextColor(Color textColor)
+		public static PlatformColor GetEntryTextColor(Color textColor)
 		{
 			return textColor != Color.Default ? ToPlatformColor(textColor) : MaterialColors.Light.PrimaryColor;
 		}
@@ -103,7 +103,7 @@ namespace Xamarin.Forms.Platform.iOS.Material
 
 		// State list from material-components-android
 		// https://github.com/material-components/material-components-android/blob/3637c23078afc909e42833fd1c5fd47bb3271b5f/lib/java/com/google/android/material/button/res/color/mtrl_btn_bg_color_selector.xml
-		public static ColorStateList CreateButtonBackgroundColors(AColor primary)
+		public static ColorStateList CreateButtonBackgroundColors(PlatformColor primary)
 		{
 			var colors = new int[] { primary, primary.WithAlpha(0.12) };
 			return new ColorStateList(ButtonStates, colors);
@@ -111,13 +111,13 @@ namespace Xamarin.Forms.Platform.iOS.Material
 
 		// State list from material-components-android
 		// https://github.com/material-components/material-components-android/blob/3637c23078afc909e42833fd1c5fd47bb3271b5f/lib/java/com/google/android/material/button/res/color/mtrl_btn_text_color_selector.xml
-		public static ColorStateList CreateButtonTextColors(AColor primary, AColor text)
+		public static ColorStateList CreateButtonTextColors(PlatformColor primary, PlatformColor text)
 		{
 			var colors = new int[] { text, primary.WithAlpha(0.38) };
 			return new ColorStateList(ButtonStates, colors);
 		}
 
-		public static ColorStateList CreateEntryFilledPlaceholderColors(AColor inlineColor, AColor floatingColor)
+		public static ColorStateList CreateEntryFilledPlaceholderColors(PlatformColor inlineColor, PlatformColor floatingColor)
 		{
 			int[][] States =
 			{
@@ -129,14 +129,14 @@ namespace Xamarin.Forms.Platform.iOS.Material
 			return new ColorStateList(States, colors);
 		}
 
-		public static ColorStateList CreateEntryUnderlineColors(AColor focusedColor, AColor unfocusedColor)
+		public static ColorStateList CreateEntryUnderlineColors(PlatformColor focusedColor, PlatformColor unfocusedColor)
 		{
 			var colors = new int[] { focusedColor, unfocusedColor };
 			return new ColorStateList(EntryUnderlineStates, colors);
 		}
 
-		internal static AColor WithAlpha(this AColor color, double alpha) =>
-			new AColor(color.R, color.G, color.B, (byte)(alpha * 255));
+		internal static PlatformColor WithAlpha(this PlatformColor color, double alpha) =>
+			new PlatformColor(color.R, color.G, color.B, (byte)(alpha * 255));
 #endif
 
 
@@ -144,19 +144,19 @@ namespace Xamarin.Forms.Platform.iOS.Material
 		{
 			// the Colors for "branding"
 			//  - we selected the "black" theme from the default DarkActionBar theme
-			public static readonly AColor PrimaryColor = FromRgb(33, 33, 33);
-			public static readonly AColor PrimaryColorVariant = AColor.Black;
-			public static readonly AColor OnPrimaryColor = AColor.White;
-			public static readonly AColor SecondaryColor = FromRgb(33, 33, 33);
-			public static readonly AColor OnSecondaryColor = AColor.White;
+			public static readonly PlatformColor PrimaryColor = FromRgb(33, 33, 33);
+			public static readonly PlatformColor PrimaryColorVariant = PlatformColor.Black;
+			public static readonly PlatformColor OnPrimaryColor = PlatformColor.White;
+			public static readonly PlatformColor SecondaryColor = FromRgb(33, 33, 33);
+			public static readonly PlatformColor OnSecondaryColor = PlatformColor.White;
 
 			// the Colors for "UI"
-			public static readonly AColor BackgroundColor = AColor.White;
-			public static readonly AColor OnBackgroundColor = AColor.Black;
-			public static readonly AColor SurfaceColor = AColor.White;
-			public static readonly AColor OnSurfaceColor = AColor.Black;
-			public static readonly AColor ErrorColor = FromRgb(176, 0, 32);
-			public static readonly AColor OnErrorColor = AColor.White;
+			public static readonly PlatformColor BackgroundColor = PlatformColor.White;
+			public static readonly PlatformColor OnBackgroundColor = PlatformColor.Black;
+			public static readonly PlatformColor SurfaceColor = PlatformColor.White;
+			public static readonly PlatformColor OnSurfaceColor = PlatformColor.Black;
+			public static readonly PlatformColor ErrorColor = FromRgb(176, 0, 32);
+			public static readonly PlatformColor OnErrorColor = PlatformColor.White;
 
 #if __IOS__
 			public static SemanticColorScheme CreateColorScheme()
@@ -184,19 +184,19 @@ namespace Xamarin.Forms.Platform.iOS.Material
 		{
 			// the Colors for "branding"
 			//  - we selected the "black" theme from the default DarkActionBar theme
-			public static readonly AColor PrimaryColor = FromRgb(33, 33, 33);
-			public static readonly AColor PrimaryColorVariant = AColor.Black;
-			public static readonly AColor OnPrimaryColor = AColor.White;
-			public static readonly AColor SecondaryColor = FromRgb(33, 33, 33);
-			public static readonly AColor OnSecondaryColor = AColor.White;
+			public static readonly PlatformColor PrimaryColor = FromRgb(33, 33, 33);
+			public static readonly PlatformColor PrimaryColorVariant = PlatformColor.Black;
+			public static readonly PlatformColor OnPrimaryColor = PlatformColor.White;
+			public static readonly PlatformColor SecondaryColor = FromRgb(33, 33, 33);
+			public static readonly PlatformColor OnSecondaryColor = PlatformColor.White;
 
 			// the Colors for "UI"
-			public static readonly AColor BackgroundColor = FromRgb(20, 20, 20);
-			public static readonly AColor OnBackgroundColor = AColor.White;
-			public static readonly AColor SurfaceColor = FromRgb(40, 40, 40);
-			public static readonly AColor OnSurfaceColor = AColor.White;
-			public static readonly AColor ErrorColor = FromRgb(194, 108, 122);
-			public static readonly AColor OnErrorColor = AColor.White;
+			public static readonly PlatformColor BackgroundColor = FromRgb(20, 20, 20);
+			public static readonly PlatformColor OnBackgroundColor = PlatformColor.White;
+			public static readonly PlatformColor SurfaceColor = FromRgb(40, 40, 40);
+			public static readonly PlatformColor OnSurfaceColor = PlatformColor.White;
+			public static readonly PlatformColor ErrorColor = FromRgb(194, 108, 122);
+			public static readonly PlatformColor OnErrorColor = PlatformColor.White;
 
 #if __IOS__
 			public static SemanticColorScheme CreateColorScheme()
@@ -221,7 +221,7 @@ namespace Xamarin.Forms.Platform.iOS.Material
 		}
 
 
-		static AColor ToPlatformColor(Color color)
+		static PlatformColor ToPlatformColor(Color color)
 		{
 #if __ANDROID__
 			return color.ToAndroid();
@@ -232,7 +232,7 @@ namespace Xamarin.Forms.Platform.iOS.Material
 
 
 
-		static AColor WithAlpha(AColor color, float alpha)
+		static PlatformColor WithAlpha(PlatformColor color, float alpha)
 		{
 #if __ANDROID__
 			return color.WithAlpha(alpha);
@@ -242,12 +242,12 @@ namespace Xamarin.Forms.Platform.iOS.Material
 		}
 
 
-		static AColor FromRgb(int red, int green, int blue)
+		static PlatformColor FromRgb(int red, int green, int blue)
 		{
 #if __ANDROID__
-			return AColor.Rgb(red, green, blue);
+			return PlatformColor.Rgb(red, green, blue);
 #else
-			return AColor.FromRGB(red, green, blue);
+			return PlatformColor.FromRGB(red, green, blue);
 
 #endif
 		}
