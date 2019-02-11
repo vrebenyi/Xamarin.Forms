69a0726fbe6fc65199c326bb03d74e031f21ded0
ShaneN <shane94@hotmail.com>
2019-02-11 13:26:03 -0700

    [visual] material editor

---
+++ Xamarin.Forms.Platform.Android/Material/MaterialEditorRenderer.cs
@@ -0,0 +1,125 @@
+﻿#if __ANDROID_28__
+using Android.Content;
+using Android.OS;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.Material;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Editor), typeof(MaterialEditorRenderer), new[] { typeof(VisualRendererMarker.Material) })]
+namespace Xamarin.Forms.Platform.Android.Material
+{
+	public sealed class MaterialEditorRenderer : EditorRendererBase<MaterialFormsTextInputLayout>
+	{
+		bool _disposed;
+		MaterialFormsEditText _textInputEditText;
+		MaterialFormsTextInputLayout _textInputLayout;
+
+		public MaterialEditorRenderer(Context context) :
+			base(MaterialContextThemeWrapper.Create(context))
+		{
+			VisualElement.VerifyVisualFlagEnabled();
+		}
+
+		IElementController ElementController => Element as IElementController;
+
+		protected override EditText EditText => _textInputEditText;
+
+		protected override MaterialFormsTextInputLayout CreateNativeControl()
+		{
+			LayoutInflater inflater = LayoutInflater.FromContext(Context);
+			var view = inflater.Inflate(Resource.Layout.TextInputLayoutFilledBox, null);
+			_textInputLayout = (MaterialFormsTextInputLayout)view;
+			_textInputEditText = _textInputLayout.FindViewById<MaterialFormsEditText>(Resource.Id.materialformsedittext);
+			_textInputLayout.Hint = Element.Placeholder;
+
+			return _textInputLayout;
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			base.OnElementChanged(e);
+			var oldElement = e.OldElement;
+
+			if (oldElement != null)
+			{
+				oldElement.PropertyChanged -= OnElementPropertyChanged;
+				oldElement.FocusChangeRequested -= OnFocusChangeRequested;
+			}
+
+			if (e.NewElement != null)
+				Element.FocusChangeRequested += OnFocusChangeRequested;
+
+			UpdateBackgroundColor();
+		}
+
+
+		protected override void OnFocusChangeRequested(object sender, VisualElement.FocusRequestArgs e)
+		{
+			e.Result = true;
+
+			if (e.Focus)
+			{
+				// use post being BeginInvokeOnMainThread will not delay on android
+				Looper looper = Context.MainLooper;
+				var handler = new Handler(looper);
+				handler.Post(() =>
+				{
+					_textInputEditText.RequestFocus();
+				});
+			}
+			else
+			{
+				_textInputEditText.ClearFocus();
+			}
+
+			if (e.Focus)
+				this.ShowKeyboard();
+			else
+				this.HideKeyboard();
+		}
+
+
+		protected override void Dispose(bool disposing)
+		{
+			if (_disposed)
+				return;
+
+			_disposed = true;
+
+			if (disposing && Element != null)
+				Element.FocusChangeRequested -= OnFocusChangeRequested;
+
+			base.Dispose(disposing);
+		}
+
+		protected override void UpdateTextColor() => ApplyTheme();
+
+		protected override void UpdateBackgroundColor()
+		{
+			if (_textInputLayout == null)
+				return;
+
+			_textInputLayout.BoxBackgroundColor = MaterialColors.CreateEntryFilledInputBackgroundColor(Element.BackgroundColor, Element.TextColor);
+		}
+
+		protected internal override void UpdatePlaceholderText()
+		{
+			_textInputLayout.Hint = Element.Placeholder;
+		}
+
+		
+		protected override void UpdatePlaceholderColor() => ApplyTheme();
+		void ApplyTheme() => _textInputLayout?.ApplyTheme(Element.TextColor, Element.PlaceholderColor);
+
+		protected internal override void UpdateFont()
+		{
+			base.UpdateFont();
+			_textInputLayout.Typeface = Element.ToTypeface();
+			_textInputEditText.SetTextSize(ComplexUnitType.Sp, (float)Element.FontSize);
+		}
+	}
+}
+#endif
